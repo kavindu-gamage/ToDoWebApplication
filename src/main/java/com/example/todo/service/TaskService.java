5c2dd095891656f124edaa242f4c1ae727873602
Kavindu <kavindu2308@gmail.com>
2023-12-12 23:29:34 +0530

    with best practices

--- src/main/java/com/example/todo/service/TaskService.java
+++ src/main/java/com/example/todo/service/TaskService.java
@@ -9,9 +9,13 @@ import com.example.todo.entity.Task;
 @Service
 public interface TaskService {
     List<Task> getAllTasks();
+
     Task getTaskById(Long id);
+
     Task saveTask(Task task);
+
     Task updateTask(Long id, Task task);
+
     void deleteTask(Long id);
 
 }
